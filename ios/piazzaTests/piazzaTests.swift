a8b8a8e5fd195fbd3f27802e5d0a852d83f2bd14
Ganesh Gunasegaran <me@itsgg.com>
2024-12-04 19:02:07 +0800

    Setup iOS

---
+++ ios/piazzaTests/piazzaTests.swift
@@ -0,0 +1,17 @@
+//
+//  piazzaTests.swift
+//  piazzaTests
+//
+//  Created by Ganesh Gunasegaran on 04/12/24.
+//
+
+import Testing
+@testable import piazza
+
+struct piazzaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
